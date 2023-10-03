3b2c69e704bda410f4dbd52c44ef0244a08a9138
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2023-10-03 16:42:40 -0300

    Menu Hamburger

---
+++ Exercicios/ex_028/mq005/script.js
@@ -0,0 +1,9 @@
+var iten = window.document.getElementById('itens')
+
+function clickMenu(){
+   if(iten.style.display == 'none'){
+    iten.style.display = 'block';
+   }else{
+    iten.style.display = 'none';
+   }
+}
